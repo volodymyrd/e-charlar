0fd496ba94926cd0835823bd0e5a7ba11e78b8f0
volodymyrd <volodymyrdotsenko@gmail.com>
2025-03-23 00:33:49 +0100

    Add broadcasting channel to emulate a chat.

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,52 @@
 use std::io::Error;
-use tokio::io::{AsyncReadExt};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
-use tokio::spawn;
+use tokio::sync::broadcast;
 
-async fn handle_client(mut stream: TcpStream) {
+async fn handle_client(stream: TcpStream, tx: broadcast::Sender<String>) {
+    let (mut reader, mut writer) = stream.into_split();
+
     let mut buffer = [0u8; 1024];
+
+    let mut rx = tx.subscribe();
+
+    tokio::spawn(async move {
+        while let Ok(msg) = rx.recv().await {
+            if (writer.write_all(msg.as_bytes()).await).is_err() {
+                break;
+            }
+        }
+    });
+
     loop {
-        let bytes_read = match stream.read(&mut buffer).await {
+        let bytes_read = match reader.read(&mut buffer).await {
             Ok(0) => break,
             Ok(n) => n,
             Err(_) => break,
         };
 
         let message = String::from_utf8_lossy(&buffer[..bytes_read]);
-        println!("Received: {}", &message[0..message.len()-1]);
+        println!("Received: {}", message);
+        if tx.send(message.into_owned()).is_err() {
+            break;
+        }
     }
 }
+
+const MAX_CLIENTS: usize = 100;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<Error>> {
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
     println!("Server listening on 127.0.0.1:8080");
 
+    let (tx, _) = broadcast::channel(MAX_CLIENTS);
+
     loop {
         match listener.accept().await {
             Ok((stream, _)) => {
-                spawn(async move { handle_client(stream).await });
+                let tx = tx.clone();
+                tokio::spawn(async move { handle_client(stream, tx).await });
             }
             Err(e) => eprintln!("Connection failed: {}", e),
         }
