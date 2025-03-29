68becfe99d5e2e4acf657ae2fb61e06215b6d457
volodymyrd <volodymyrdotsenko@gmail.com>
2025-03-29 01:28:41 +0100

    add anyhow to improve readability.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use futures::{SinkExt, StreamExt};
 use std::collections::HashMap;
-use std::io::Error;
 use std::sync::Arc;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::{Mutex, broadcast};
 use tokio_util::codec::{FramedRead, FramedWrite, LinesCodec};
 
-async fn handle_client(stream: TcpStream, tx: broadcast::Sender<(i64, String)>, users: UserMap) {
+async fn handle_client(
+    stream: TcpStream,
+    tx: broadcast::Sender<(i64, String)>,
+    users: UserMap,
+) -> anyhow::Result<()> {
     let (reader, writer) = stream.into_split();
 
     let client_id = rand::random::<i64>();
@@ -15,15 +18,12 @@ async fn handle_client(stream: TcpStream, tx: broadcast::Sender<(i64, String)>, 
     let mut sink = FramedWrite::new(writer, LinesCodec::new());
 
     // Request username
-    if (sink.send("Enter your username: ").await).is_err() {
-        return;
-    }
+    sink.send("Enter your username: ").await?;
+
     if let Some(Ok(username)) = stream.next().await {
         users.lock().await.insert(client_id, username.clone());
         println!("{} has joined the chat.", username);
-        if (sink.send(format!("Hello {username}! ❤️")).await).is_err() {
-            return;
-        }
+        sink.send(format!("Hello {username}! ❤️")).await?;
     }
 
     let mut rx = tx.subscribe();
@@ -36,29 +36,20 @@ async fn handle_client(stream: TcpStream, tx: broadcast::Sender<(i64, String)>, 
                 match user_message {
                     None => break,
                     Some(user_message) => {
-                        if user_message.is_err() {
-                            break;
-                        }
-                        let user_message  = user_message.unwrap();
+                        let user_message  = user_message?;
                         println!("Received: {}", user_message.trim());
-                        if tx.send((client_id, user_message)).is_err() {
-                            break;
-                        }
+                        tx.send((client_id, user_message))?
                     },
                 };
             },
             recv = rx.recv() => {
-                if recv.is_err() {
-                    break;
-                }
-                let (id, peer_message) = recv.unwrap();
+                let (id, peer_message) = recv?;
                 if client_id != id {
-                    let sender_name = users_clone.lock().await.get(&id)
+                    let sender_name = users_clone.lock().await
+                    .get(&id)
                     .unwrap_or(&"Unknown".to_string()).clone();
                     let formatted_msg = format!("{}: {}", sender_name, peer_message);
-                    if (sink.send(formatted_msg).await).is_err() {
-                        break;
-                    }
+                    sink.send(formatted_msg).await?;
                 }
             }
         }
@@ -66,13 +57,15 @@ async fn handle_client(stream: TcpStream, tx: broadcast::Sender<(i64, String)>, 
 
     let username = users.lock().await.remove(&client_id).unwrap();
     println!("{} has left the chat.", username);
+
+    Ok(())
 }
 
 type UserMap = Arc<Mutex<HashMap<i64, String>>>;
 const MAX_CLIENTS: usize = 100;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<Error>> {
+async fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
     println!("Server listening on 127.0.0.1:8080");
 
